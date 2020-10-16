d6c69244e9b4671ccff102445d0f080536c6e970
吴吉央 <475038870@qq.com>
2020-10-16 10:57:06 +0800

    测试自动建表和commit功能

---
+++ src/main/java/com/api/market/Entity/User.java
@@ -0,0 +1,29 @@
+package com.api.market.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    private Integer id;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
